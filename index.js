a963a5efaf24b2717c4bf428478686d4156d140b
paljor129 <simplonco@debian>
2017-02-16 15:35:15 +0100

    js-code

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-var myPlats = document.getElementById("containerPlats");
+var head = document.createElement("div");
+var btnPan = document.createElement("BUTTON");
+console.log(btnPan);
+var panierTxt = document.createTextNode("PANIER")
+console.log(panierTxt);
+// var btnTxt = document.createElement("p");
+// btnTxt.innerHTML = "<p>panier</p>";
+head.appendChild(btnPan);
+btnPan.appendChild(panierTxt);
+document.body.appendChild(head);
+btnPan.style.float = "right";
+head.style.margin = "20px";
+
+var main = document.createElement("div");
+main.classList.add("containerPlats");
+document.body.appendChild(main);
+var myPlats = document.getElementsByClassName("containerPlats");
 var content = table.plats;
 var box = [];
 
@@ -11,18 +27,51 @@ function myMenu(plats){
     var price = document.createElement("p");
     var selected = document.createElement("p");
 
+    var btn = document.createElement("BUTTON");
+    console.log(btn);
+    var btnTxt = document.createTextNode("Order")
+    console.log(btnTxt);
+
     id.innerHTML = plats[i].id;
     nom.innerHTML = plats[i].nom;
     description.innerHTML = plats[i].description;
     price.innerHTML = plats[i].price;
     selected.innerHTML = plats[i].selected;
+    btn.innerHTML;
 
-    containerPlats.appendChild(div);
+    main.style.display = "flex";
+    main.style.justifyContent = "center";
+    // main.style.float = "none"
+    div.style.textAlign = "center";
+    div.style.height = "250px";
+    div.style.width = "200px";
+    div.style.backgroundColor = "blue";
+    div.style.color = "white";
+    div.style.margin = "30px";
+    btn.style.backgroungColor = "yellow";
+
+
+    main.appendChild(div);
     div.appendChild(id);
     div.appendChild(nom);
     div.appendChild(description);
     div.appendChild(price);
     div.appendChild(selected);
+    btn.appendChild(btnTxt);
+    div.appendChild(btn);
+
   }
 }
 myMenu(content);
+
+
+
+
+
+// $("#myButton").click(function () {
+//     var test = $('<button>Test</button>').click(function () {
+//         alert('hi');
+//     });
+    // $("#nodeAttributeHeader").attr('style', 'display: table-row;');
+    // $("#addNodeTable tr:last").before('<tr><td>' + test.html() + '</td></tr>');
+// });
